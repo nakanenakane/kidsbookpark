ff3278fbbf8911c4c87c06cb664eb0a6e1a6aa31
nakanenakane <nandakakanashe@gmail.com>
2021-05-27 00:24:22 +0900

    devise user controller編集

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,38 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(sign_up_params)
+    if @user.save
+      session[:user_id] = @user.id
+      session["devise.regist_data"] = {user: @user.attributes}
+     redirect_to users_addresses_path
+    else
+      render "new"
+    end
+  end
+
+  def new_address
+    @address = Address.new
+  end
+  
+  def create_address
+    @user = User.new(session["devise.regist_data"]["user"])
+    @address = Address.new(address_params)
+    if @address.save
+      sign_in(@user)
+      redirect_to root_path
+    else
+      render "new_address"
+    end
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:address_family_name, :address_first_name, :address_family_name_kana, :address_first_name_kana, :prefecture_id, :post_code, :city, :address_line, :building_name, :phone).merge(user_id: session[:user_id])
+  end
+end
