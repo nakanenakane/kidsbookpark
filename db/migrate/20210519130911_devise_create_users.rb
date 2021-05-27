14676139261b5687cdd46e8a1f767705cfbff083
nakanenakane <nandakakanashe@gmail.com>
2021-05-27 23:44:52 +0900

    userDBカラム追加

--- db/migrate/20210519130911_devise_create_users.rb
+++ db/migrate/20210519130911_devise_create_users.rb
@@ -6,6 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :first_name,   null: false
+      t.string :family_name,   null: false
+      t.string :first_name_kana,   null: false
+      t.string :family_name_kana,   null: false
+      t.date :birthday
+      t.string :introduction
 
       ## Recoverable
       t.string   :reset_password_token
