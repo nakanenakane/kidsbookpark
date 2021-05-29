042b0955860b74576a7c365980ec791c500b0dcd
nakanenakane <nandakakanashe@gmail.com>
2021-05-29 23:13:36 +0900

    book model編集

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,7 +6,7 @@ class Book < ApplicationRecord
   accepts_nested_attributes_for :item_images, allow_destroy: true
   belongs_to :user
   belongs_to :category
-  belongs_to_active_hash :publisher_code
+  belongs_to_active_hash :publisher
   belongs_to_active_hash :condition
   belongs_to_active_hash :preparation_day
   belongs_to_active_hash :postage_payer
@@ -15,7 +15,7 @@ class Book < ApplicationRecord
   validates :name, presence: true
   validates :introduction, presence: true
   validates :category_id, presence: true
-  validates :publisher_code_id, presence: true
+  validates :publisher_id, presence: true
   validates :condition_id, presence: true
   validates :preparation_day_id, presence: true
   validates :postage_payer_id, presence: true
