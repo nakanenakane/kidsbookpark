299f009322b9f040e95fdd935d5153855ca32783
nakanenakane <nandakakanashe@gmail.com>
2021-05-29 23:12:19 +0900

    発送日編集

--- app/models/preparation_day.rb
+++ app/models/preparation_day.rb
@@ -1,5 +1,5 @@
 class PreparationDay < ActiveHash::Base
   self.data = [
-      {id: 1, preparation_day: '1〜2で発送'}, {id: 2, preparation_day: '2〜7で発送'}, {id: 3, preparation_day: '4〜7で発送'}
+      {id: 1, preparation_day: '1〜2日で発送'}, {id: 2, preparation_day: '2〜7日で発送'}, {id: 3, preparation_day: '4〜7日で発送'}
   ]
 end
