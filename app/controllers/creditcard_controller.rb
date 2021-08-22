1ff399c4bc8fe7a9fd4396d750297aad06e0c3dc
nakanenakane <nandakakanashe@gmail.com>
2021-08-22 16:52:50 +0900

    creditcard関連

---
+++ app/controllers/creditcard_controller.rb
@@ -0,0 +1,74 @@
+class CreditcardController < ApplicationController
+end
+class CardController < ApplicationController
+
+  require "payjp"
+  before_action :set_card
+
+  def index
+  end
+
+  def new
+    @card = CreditCard.new
+  end
+
+  def create
+    Payjp.api_key = Rails.application.credentials.dig(:payjp, :secret_key)
+    if params['payjp-token'].present?
+      customer = Payjp::Customer.create(
+        email: current_user.email,
+        card: params['payjp-token'],
+        metadata: {user_id: current_user.id}
+      )
+      @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+        redirect_to mypages_path(current_user.id)
+      else
+        render :new
+      end
+    else
+      render :new
+    end
+  end
+
+  def delete
+    if @card.present?
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :secret_key)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      customer.delete
+      @card.delete
+    end
+    redirect_to mypages_path(current_user.id)
+  end
+
+  def show 
+    if @card.present?
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :secret_key)
+      customer = Payjp::Customer.retrieve(@card.customer_id) 
+      @default_card_information = customer.cards.retrieve(@card.card_id)
+      @card_brand = @default_card_information.brand
+      case @card_brand
+      when "Visa"
+        @card_src = "visa.png"
+      when "JCB"
+        @card_src = "jcb.jpeg"
+      when "MasterCard"
+        @card_src = "master.png"
+      when "American Express"
+        @card_src = "amex.png"
+      when "Diners Club"
+        @card_src = "diners.gif"
+      when "Discover"
+        @card_src = "discover.jpg"
+      end
+    else
+      redirect_to action: "new"
+    end
+ end
+
+  private
+
+  def set_card
+    @card = CreditCard.find_by(user_id: current_user)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
      get 'address', to: 'mypages#my_address'
    end
   end
+
   resources :books, only: [:index, :show, :new, :create, :edit, :destroy]do
     resources :comments,  only: [:create, :destroy]
     collection do
@@ -21,6 +22,16 @@ Rails.application.routes.draw do
       get 'get_category_children', defaults: { fomat: 'json'}
     end
   end
+
+  resources :creditcard, only: [:create, :show, :edit, :new, :destroy] do
+    collection do
+      post 'delete', to: 'card#delete'
+      get 'registration', to:'card#registration'
+    end
+     member do
+      get 'show', to: 'card#show'
+    end
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'front#index'
 end
